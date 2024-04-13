c0e2bdfafc2d84d53c6381baba848b530da15a1d
NancyPatel11 <nancypatel5757@gmail.com>
2024-04-13 17:08:14 +0530

    updated api and removed old model

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ import PIL
 from PIL import Image
 
 #import .h5 model
-model = tf.keras.models.load_model('model_trained_vg16.h5')
+model = tf.keras.models.load_model('model_trained_effnet.h5')
 
 app = FastAPI()
 
@@ -26,13 +26,13 @@ async def create_upload_file(file: UploadFile = File(...)):
     
     # Convert the content into a bytes stream
     img_stream = io.BytesIO(contents)
-    img = Image.open(img_stream).resize((384, 384))
+    img = Image.open(img_stream).resize((224, 224))
 
     # img = tf.keras.preprocessing.image.load_img(file_img, target_size=(200, 200))
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
     predictions = model.predict(img_array)
-    garbage_types = [ 'battery', 'biological', 'brown-glass', 'cardboard','cloths', 'green-glass','metal', 'paper', 'plastic','shoes' ,'trash', 'white-glass']
+    garbage_types = [ 'battery', 'biological', 'brown-glass', 'cardboard','clothes', 'green-glass','metal', 'paper', 'plastic','shoes' ,'trash', 'white-glass']
     predicted_class = garbage_types[np.argmax(predictions)]
     # print(f"The predicted class is: {predicted_class}")
     #give confidence score
